e019f252cd82347dc2a19a73944cdcf3b8d43490
Raquel Bonilla <raquelb.2014@gmail.com>
2018-10-05 14:27:41 -0700

    init proj

---
+++ src/sumTwoNum/sumNum.java
@@ -0,0 +1,21 @@
+package sumTwoNum;
+
+public class sumNum {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		
+		int sum = addNum(a,b);
+		
+		System.out.println("sum is: " + sum);
+	}
+	
+	public static int addNum(int num1, int num2) {
+		int sum;
+		
+		sum = num1 + num2;
+		
+	    
+		return sum;
+	  }
+}
